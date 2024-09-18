e2b40468d10340661c317f1eddecd09ad838fc42
Moontean <alexandru.muntean1@isa.utm.md>
2024-09-18 10:25:26 +0300

    step first

---
+++ actions/get-colors.tsx
@@ -0,0 +1,10 @@
+import { Color } from "@/types/types"
+
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
+
+export const getColors = async (): Promise<Color[]> => {
+  const res = await fetch(URL)
+
+  return res.json()
+}
